cb3a3e3b29aed2edf56f5380d35841097763583c
biifounder <biifounder@gmail.com>
2024-07-09 10:36:14 +0100

    st

--- django_sample/settings.py
+++ django_sample/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'courses/static/'
-
+# STATIC_URL = 'static/'
+STATIC_URL = '/var/www/django_app/django_sample/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -134,7 +134,7 @@ STATIC_URL = 'courses/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'courses/static/')
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
