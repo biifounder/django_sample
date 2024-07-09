5725dee3d0d3bf30de380c3c241bc8010031d5cd
biifounder <biifounder@gmail.com>
2024-07-09 10:56:45 +0100

    st

--- django_sample/settings.py
+++ django_sample/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATIC_URL = '/var/www/django_app/django_sample/static/'
+STATIC_URL = '/var/www/django_app/django_sample/courses/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
