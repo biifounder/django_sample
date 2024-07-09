0a3cef561342e0f281fee5827397e0067c73026e
biifounder <biifounder@gmail.com>
2024-07-09 10:59:30 +0100

    st

--- django_sample/settings.py
+++ django_sample/settings.py
@@ -126,16 +126,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATIC_URL = '/var/www/django_app/django_sample/courses/static/'
+# STATIC_URL = '/var/www/django_app/django_sample/courses/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)).strip('ifounder')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-# STATIC_ROOT = os.path.join(BASE_DIR, 'courses/static/')
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)).strip('ifounder')
